13602b493b6e4096988d234459dad776fba5e3be
rwecho <rwecho@live.com>
2021-08-23 12:15:41 +0800

    intialize project

---
+++ WpfLogs/LogsViewer.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfLogs
+{
+    /// <summary>
+    /// Interaction logic for LogsViewer.xaml
+    /// reference https://stackoverflow.com/questions/16743804/implementing-a-log-viewer-with-wpf
+    /// </summary>
+    public partial class LogsViewer : UserControl
+    {
+        public ObservableCollection<string> LogEntries
+        {
+            get { return (ObservableCollection<string>)GetValue(LogEntriesProperty); }
+            set { SetValue(LogEntriesProperty, value); }
+        }
+
+        public Timer Timer { get; }
+
+        public static readonly DependencyProperty LogEntriesProperty =
+            DependencyProperty.Register("LogEntries", typeof(ObservableCollection<string>), typeof(LogsViewer), new PropertyMetadata(new ObservableCollection<string>()));
+
+        public uint Buffer
+        {
+            get { return (uint)GetValue(BufferProperty); }
+            set { SetValue(BufferProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyNumber.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BufferProperty =
+            DependencyProperty.Register("Buffer", typeof(uint), typeof(LogsViewer), new PropertyMetadata(10000U));
+
+
+        public LogsViewer()
+        {
+            InitializeComponent();
+
+            this.Timer = new Timer(this.Timer_Elapsed, null, 1000, 10);
+        }
+
+        private void Timer_Elapsed(object? state)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                if (this.LogEntries.Count > this.Buffer)
+                {
+                    for (int i = 0; i < 1.0 / this.Buffer; i++)
+                    {
+                        this.LogEntries.RemoveAt(i);
+                    }
+                }
+            });
+        }
+
+        private bool AutoScroll = true;
+
+        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            if (e.Source is not ScrollViewer scrollViewer)
+            {
+                return;
+            }
+
+            // User scroll event : set or unset autoscroll mode
+            if (e.ExtentHeightChange == 0)
+            {   // Content unchanged : user scroll event
+                if (scrollViewer.VerticalOffset == scrollViewer.ScrollableHeight)
+                {   // Scroll bar is in bottom
+                    // Set autoscroll mode
+                    AutoScroll = true;
+                }
+                else
+                {   // Scroll bar isn't in bottom
+                    // Unset autoscroll mode
+                    AutoScroll = false;
+                }
+            }
+
+            // Content scroll event : autoscroll eventually
+            if (AutoScroll && e.ExtentHeightChange != 0)
+            {   // Content changed and autoscroll mode set
+                // Autoscroll
+                scrollViewer.ScrollToVerticalOffset(scrollViewer.ExtentHeight);
+            }
+        }
+    }
+}
---
+++ WpfLogs/Window1.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfLogs
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private string TestData = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+
+        private Random Random { get; } = new Random();
+        public Window1()
+        {
+            InitializeComponent();
+            Words = TestData.Split(' ').ToList();
+            Maxword =  Words.Count - 1;
+            this.Logs = new ObservableCollection<string>();
+
+            this.Number = 10000;
+            this.Timer = new Timer(this.Timer_Elapsed, null, 1000,10);
+            this.DataContext = this;
+        }
+
+        private void Timer_Elapsed(object? state)
+        {
+            Dispatcher.BeginInvoke((Action)(() => Logs.Add(GetRandomEntry())));
+        }
+
+        private string GetRandomEntry()
+        {
+            var log = string.Join(" ", Enumerable.Range(5, Random.Next(10, 50))
+                                                      .Select(x => Words[Random.Next(0, Maxword)]));
+
+            return log;
+        }
+
+        public List<string> Words { get; }
+
+        public int Number { get; set; }
+
+        private readonly int Maxword;
+
+        public ObservableCollection<string> Logs { get; }
+        public Timer Timer { get; }
+    }
+}
